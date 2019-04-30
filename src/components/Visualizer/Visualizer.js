0e6613b51094a9e1c20b03aaca60563c5de8ea2b
Will Howes <willmhowes@gmail.com>
2019-04-30 11:15:54 -0500

    organize code for better readability

--- src/components/Visualizer/Visualizer.js
+++ src/components/Visualizer/Visualizer.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Placeholder, Segment } from 'semantic-ui-react';
+import { Placeholder, Segment, Header } from 'semantic-ui-react';
 import { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import { Bar } from 'react-chartjs-2';
+import { Pie } from 'react-chartjs-2';
 import './Visualizer.css';
 
 class Visualizer extends Component {
 
    renderGraph = (dataset, i) => {
-      let tableData = [dataset['American Indian or Alaska Native'],
-      dataset['Asian'], dataset['Hawaiian/ Pacific Islander'],
-      dataset['Hispanic'], dataset['Black'], dataset['White'],
-      dataset['Two or more races']];
+      let tableData = [
+         dataset['American Indian or Alaska Native'],
+         dataset['Asian'],
+         dataset['Hawaiian/ Pacific Islander'],
+         dataset['Hispanic'],
+         dataset['Black'],
+         dataset['White'],
+         dataset['Two or more races']
+      ];
+
       console.log('dataset', i, '--', tableData);
 
       let data = {
-         labels: ["American Indian or Alaska Native", "Asian",
-            "Hawaiian/ Pacific Islander", "Hispanic",
-            "Black", "White", "Two or more races"],
+         labels: [
+            "American Indian or Alaska Native",
+            "Asian",
+            "Hawaiian/ Pacific Islander",
+            "Hispanic",
+            "Black",
+            "White",
+            "Two or more races"
+         ],
          datasets: [{
             label: dataset['Category'],
             data: tableData,
@@ -29,7 +41,9 @@ class Visualizer extends Component {
                'rgba(75, 192, 192, 0.2)',
                'rgba(153, 102, 255, 0.2)',
                'rgba(255, 159, 64, 0.2)',
-               'rgba(245, 134, 23, 0.2)'
+               'rgba(245, 134, 23, 0.2)',
+               'rgba(255, 99, 132, 0.2)',
+               'rgba(54, 162, 235, 0.2)',
             ],
             borderColor: [
                'rgba(255, 99, 132, 1)',
@@ -38,7 +52,9 @@ class Visualizer extends Component {
                'rgba(75, 192, 192, 1)',
                'rgba(153, 102, 255, 1)',
                'rgba(255, 159, 64, 1)',
-               'rgba(245, 134, 23, 1)'
+               'rgba(245, 134, 23, 1)',
+               'rgba(255, 99, 132, 1)',
+               'rgba(54, 162, 235, 1)',
             ],
             borderWidth: 1
          }]
@@ -46,7 +62,7 @@ class Visualizer extends Component {
 
       return (
          <Segment>
-            <Bar
+            <Pie
                data={data}
                // width={400}
                height={400}
@@ -75,7 +91,6 @@ class Visualizer extends Component {
             </Placeholder>
          );
       } else {
-         // let tableLabel = this.props.dataset.map(x => x['Category']);
          return (this.props.dataset.map((datarow, i) =>
             <div className="Visualizer-div-graph">
                {this.renderGraph(datarow, i)}
@@ -87,7 +102,11 @@ class Visualizer extends Component {
    render() {
       return (
          <section>
-            {this.handleGraphRender()}
+            <Header as="h1">
+               Login
+            </Header>
+
+            {/* {this.handleGraphRender()} */}
             <p>{JSON.stringify(this.props.dataset)}</p>
          </section >
       );
@@ -100,22 +119,3 @@ const mapStateToProps = state => ({
 });
 
 export default connect(mapStateToProps)(withRouter(Visualizer));
-
-
-// [`chartData${i}`]: {
-//    labels: ["American Indian or Alaska Native", "Asian",
-//       "Hawaiian/ Pacific Islander", "Hispanic",
-//       "Black", "White", "Two or more races"],
-//       datasets: [{
-//          label: category['Category'],
-//          data: [
-//             category['American Indian or Alaska Native'],
-//             category['Asian'],
-//             category['Hawaiian/ Pacific Islander'],
-//             category['Hispanic'],
-//             category['Black'],
-//             category['White'],
-//             category['Two or more races'],
-//          ],
-//       }],
-// }
