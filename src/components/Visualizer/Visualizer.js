da086b5f2ed6fbb2fe2f81d9e4650d501c29f698
Will Howes <willmhowes@gmail.com>
2019-04-30 10:12:46 -0500

    make so data is visualized on DOM

--- src/components/Visualizer/Visualizer.js
+++ src/components/Visualizer/Visualizer.js
@@ -1,14 +1,73 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Placeholder } from 'semantic-ui-react';
+import { Placeholder, Segment } from 'semantic-ui-react';
 import { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import { Pie } from 'react-chartjs-2';
-
+import { Bar } from 'react-chartjs-2';
+import './Visualizer.css';
 
 class Visualizer extends Component {
 
-   renderGraph = () => {
+   renderGraph = (dataset, i) => {
+      let tableData = [dataset['American Indian or Alaska Native'],
+      dataset['Asian'], dataset['Hawaiian/ Pacific Islander'],
+      dataset['Hispanic'], dataset['Black'], dataset['White'],
+      dataset['Two or more races']];
+      console.log('dataset', i, '--', tableData);
+
+      let data = {
+         labels: ["American Indian or Alaska Native", "Asian",
+            "Hawaiian/ Pacific Islander", "Hispanic",
+            "Black", "White", "Two or more races"],
+         datasets: [{
+            label: dataset['Category'],
+            data: tableData,
+            backgroundColor: [
+               'rgba(255, 99, 132, 0.2)',
+               'rgba(54, 162, 235, 0.2)',
+               'rgba(255, 206, 86, 0.2)',
+               'rgba(75, 192, 192, 0.2)',
+               'rgba(153, 102, 255, 0.2)',
+               'rgba(255, 159, 64, 0.2)',
+               'rgba(245, 134, 23, 0.2)'
+            ],
+            borderColor: [
+               'rgba(255, 99, 132, 1)',
+               'rgba(54, 162, 235, 1)',
+               'rgba(255, 206, 86, 1)',
+               'rgba(75, 192, 192, 1)',
+               'rgba(153, 102, 255, 1)',
+               'rgba(255, 159, 64, 1)',
+               'rgba(245, 134, 23, 1)'
+            ],
+            borderWidth: 1
+         }]
+      };
+
+      return (
+         <Segment>
+            <Bar
+               data={data}
+               // width={400}
+               height={400}
+               options={{
+                  maintainAspectRatio: false,
+                  legend: {
+                     display: true,
+                     position: 'right',
+                  },
+                  title: {
+                     display: true,
+                     text: dataset['Category'],
+                     fontSize: 25,
+                  }
+               }}
+            />
+         </Segment>
+      );
+   }
+
+   handleGraphRender = () => {
       if (this.props.dataset[0] === "Data") {
          return (
             <Placeholder style={{ height: 150, width: 150 }}>
@@ -16,57 +75,20 @@ class Visualizer extends Component {
             </Placeholder>
          );
       } else {
-         let tableLabel = this.props.dataset.map(x => x['Category']);
-         let dataset = this.props.dataset[4];
-         let tableData = [dataset['American Indian or Alaska Native'],
-            dataset['Asian'], dataset['Hawaiian/ Pacific Islander'],
-            dataset['Hispanic'], dataset['Black'], dataset['White'],
-            dataset['Two or more races']];
-
-         let data = {
-            labels: ["American Indian or Alaska Native", "Asian",
-               "Hawaiian/ Pacific Islander", "Hispanic",
-               "Black", "White", "Two or more races"],
-            datasets: [{
-               label: `${dataset['Category']}`,
-               data: tableData,
-               backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-               ],
-               borderColor: [
-                  'rgba(255, 99, 132, 1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)',
-                  'rgba(255, 159, 64, 1)'
-               ],
-               borderWidth: 1
-            }]
-         };
-
-         return (
-            <Pie
-               data={data}
-               width={400}
-               height={400}
-               options={{ maintainAspectRatio: false }}
-            />
-         );
+         // let tableLabel = this.props.dataset.map(x => x['Category']);
+         return (this.props.dataset.map((datarow, i) =>
+            <div className="Visualizer-div-graph">
+               {this.renderGraph(datarow, i)}
+            </div>
+         ));
       }
    }
 
    render() {
       return (
          <section>
-            {this.renderGraph()}
+            {this.handleGraphRender()}
+            <p>{JSON.stringify(this.props.dataset)}</p>
          </section >
       );
    }
