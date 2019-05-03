c4a9305472602e283b1adaea62f87990f5f8cf1d
Will Howes <willmhowes@gmail.com>
2019-05-03 11:56:33 -0500

    make so URL contains needed information to reconstruct selection bars

--- src/components/Visualizer/Visualizer.js
+++ src/components/Visualizer/Visualizer.js
@@ -23,14 +23,36 @@ class Visualizer extends Component {
    getUrl = () => {
       // Breaking down URL into an array pathSplit
       const pathSplit = window.location.hash.split('/')
+      console.log(pathSplit);
 
       // Retrieving scope points from URL
       const currentScope = String(pathSplit[2]);
-      const scopeIdentity = String(pathSplit[3]);
-      const datasetYearValue = String(pathSplit[5]);
+      const datasetYearValue = String(pathSplit[4]);
+      const stateValue = String(pathSplit[5]);
+      let districtValue = null;
+      let schoolValue = null;
+
+      // Parse URL for districtValue and schoolValue
+      // This works because first 7 digits of SchoolID are the District ID
+      if (String(pathSplit[6]).length === 7) {
+         districtValue = String(pathSplit[6]);
+      } else if (String(pathSplit[6]).length === 12) {
+         districtValue = String(pathSplit[6]).substring(0, 7);
+         schoolValue = String(pathSplit[6]);
+      }
+
+      // save current scope's relevant value in a variable scopeIdentity
+      let scopeIdentity;
+      if (currentScope === 'state') {
+         scopeIdentity = stateValue;
+      } else if (currentScope === 'district') {
+         scopeIdentity = districtValue;
+      } else if (currentScope === 'school') {
+         scopeIdentity = schoolValue;
+      }
 
       // Rebuilding dataset name from URL
-      const datasetValueMod = String(pathSplit[4]);
+      const datasetValueMod = String(pathSplit[3]);
       const datasetValueArray = datasetValueMod.split('_');
       const datasetValue = datasetValueArray.join(' ');
 
@@ -137,24 +159,24 @@ class Visualizer extends Component {
    render() {
       return (
          <section>
-            {/* <Header as="h3" textAlign='left'>
+            <Header as="h3" textAlign='left'>
                {this.props.datasetValue}
-            </Header> */}
+            </Header>
 
             {/* <RenderStateInput isRequired={true} />
             <RenderDistrictInput />
             <RenderSchoolInput /> */}
 
-            {/* <Breadcrumb>
+            <Breadcrumb>
                <Breadcrumb.Section link>Home</Breadcrumb.Section>
                <Breadcrumb.Divider />
                <Breadcrumb.Section link>Store</Breadcrumb.Section>
                <Breadcrumb.Divider icon='right angle' />
                <Breadcrumb.Section active>
                   Search for:
-                  <a href='#'>paper towels</a>
+                  {/* <a href='#'>paper towels</a> */}
                </Breadcrumb.Section>
-            </Breadcrumb> */}
+            </Breadcrumb>
 
             {this.handleGraphRender()}
          </section >
